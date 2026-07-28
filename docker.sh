09f9ef7238fa8a144ad8a444897326d1890343b6
LinCenYing <lincenying@qq.com>
2026-07-28 22:28:25 +0800

    chore: update

---
+++ docker.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-mongodb:1.26.0728
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-mongodb:1.26.0728 lincenying/api-bun-mongodb:1.26.0728
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-mongodb:1.26.0728
+
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0729
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0729 lincenying/api-bun-postgre:1.26.0729
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0729
+
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0728
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0728 lincenying/api-express:1.26.0728
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0728
+
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0727
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0727 lincenying/app-nuxt:1.26.0727
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0727
+
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0727
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0727 lincenying/app-php:1.26.0727
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0727
+
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-vue3-ssr:1.26.0727
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-vue3-ssr:1.26.0727 lincenying/app-vue3-ssr:1.26.0727
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-vue3-ssr:1.26.0727
+
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0727
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0727 lincenying/nginx-php:1.26.0727
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0727
