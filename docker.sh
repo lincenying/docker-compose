bf1c92786c1edcab40b0477ac2d9284ccbe801f4
LinCenYing <lincenying@qq.com>
2026-08-03 17:58:00 +0800

    chore: update

--- docker.sh
+++ docker.sh
@@ -4,13 +4,13 @@ docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bu
 docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-mongodb:1.26.0728 lincenying/api-bun-mongodb:1.26.0728
 docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-mongodb:1.26.0728
 
-docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0731
-docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0731 lincenying/api-bun-postgre:1.26.0731
-docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0731
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0804
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0804 lincenying/api-bun-postgre:1.26.0804
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-bun-postgre:1.26.0804
 
-docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0728
-docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0728 lincenying/api-express:1.26.0728
-docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0728
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0803
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0803 lincenying/api-express:1.26.0803
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0803
 
 docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0727
 docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0727 lincenying/app-nuxt:1.26.0727
