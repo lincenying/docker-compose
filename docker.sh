98b4cf9750e6bd16b5ba3d9d016e99d12f550bc8
LinCenYing <lincenying@qq.com>
2026-08-29 23:58:08 +0800

    chore: update

--- docker.sh
+++ docker.sh
@@ -12,21 +12,21 @@ docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-ex
 docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0803 lincenying/api-express:1.26.0803
 docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/api-express:1.26.0803
 
-docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0727
-docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0727 lincenying/app-nuxt:1.26.0727
-docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0727
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0806
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0806 lincenying/app-nuxt:1.26.0806
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-nuxt:1.26.0806
 
-docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0727
-docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0727 lincenying/app-php:1.26.0727
-docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0727
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0829
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0829 lincenying/app-php:1.26.0829
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-php:1.26.0829
 
 docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-vue3-ssr:1.26.0731
 docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-vue3-ssr:1.26.0731 lincenying/app-vue3-ssr:1.26.0731
 docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/app-vue3-ssr:1.26.0731
 
-docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0727
-docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0727 lincenying/nginx-php:1.26.0727
-docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0727
+docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0829
+docker tag swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0829 lincenying/nginx-php:1.26.0829
+docker rmi swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/lincenying/nginx-php:1.26.0829
 
 docker pull swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/crazymax/fail2ban:1.1.0
 docker tag  swr.cn-north-4.myhuaweicloud.com/ddn-k8s/docker.io/crazymax/fail2ban:1.1.0 crazymax/fail2ban:1.1.0
